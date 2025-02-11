2bffe3566605362a82ed075228ba4b8923f04814
René Hoffmann <rene.hoffmann@bertelsmann.de>
2025-02-11 12:46:47 +0100

    add favicon

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Word-Guessing-Game
+
+This is just a little game for playing around and learning how to code.
--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const word_original = words[Math.floor(Math.random() * words.length)]
-let word_disguised = word_original.replace(/\w/g, "_")
+let word_disguised = word_original.replace(/\w/g, "⭑")
 const unique_characters_array = [...new Set(word_original.replace(/[^a-zA-Z]/g, ""))]
+const unique_consonants_array = [...new Set(word_original.replace(/[aeiouAEIOU]/g, ""))]
+const unique_consonants_count = unique_consonants_array.length
 const unique_characters_count = unique_characters_array.length
 
-const failures_allowed = 5 + unique_characters_count
+const failures_allowed = 5 + Math.ceil(unique_consonants_count**.7)
 let failures_done = 0
 
 console.log(word_original)
+document.getElementById("html__hint").innerHTML = `Hint: The word we are looking for consists of ${word_original.length} letters. ${unique_characters_array.length} of them are unique.`
 document.getElementById("html__word_disguised").innerHTML = word_disguised
-document.getElementById("html__error_rate").innerHTML = `${failures_done}/${failures_allowed}`
+document.getElementById("html__error_rate").innerHTML = `Errors: ${failures_done}/${failures_allowed}`
 
 let userInput = ""
+let userInputs = ""
 function get_input() {
     userInput = document.getElementById("html__input").value.toLowerCase()
     document.getElementById("html__input").value = ""
-    document.getElementById("html__history").innerHTML += userInput
+
+    // Prevent testing repeated imputs of the same character
+    if (userInputs.includes(userInput)) {
+        return
+    }
+
+    userInputs += userInput
+    document.getElementById("html__history").innerHTML += `${userInput} `
 
     if (word_original.toLowerCase().includes(userInput)) {
         for (let index = 0; index < word_original.length; index++) {
@@ -26,10 +37,15 @@ function get_input() {
         }
         document.getElementById("html__word_disguised").innerHTML = word_disguised
         if (word_disguised === word_original) {
-            document.getElementById("html__body").innerHTML = `<h1 class="success">SUCCESS</h1><p class="success">${word_original}</p>`
+            document.getElementById("html__body").innerHTML = `
+                <h1 class="success">SUCCESS</h1>
+                <p class="success solution">${word_original}</p>
+                <p>You missed ${failures_done} of ${failures_allowed} allowed.</p>
+                <button onclick="location.reload()">New Game</button>
+            `
         }
     } else {
-        document.getElementById("html__error_rate").innerHTML = `${++failures_done}/${failures_allowed}`
+        document.getElementById("html__error_rate").innerHTML = `Errors: ${++failures_done}/${failures_allowed}`
     }
     if (failures_done === failures_allowed) {
         let evaluation = ""
@@ -40,6 +56,10 @@ function get_input() {
                 evaluation += `<span class="fail">${word_original[index]}</span>`
             }
         }
-        document.getElementById("html__body").innerHTML = `<h1 class="fail">FAIL</h1><p>${evaluation}</p></p>`
+        document.getElementById("html__body").innerHTML = `
+            <h1 class="fail">FAIL</h1>
+            <p class="solution">${evaluation}</p>
+            <button onclick="location.reload()">New Game</button>
+        `
     }
 }
